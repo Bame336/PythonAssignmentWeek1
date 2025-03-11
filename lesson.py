d0568a8e29082a4e92dad328f96902751ed6d7cc
Arowolo Oluwamodupe <115076427+Bame336@users.noreply.github.com>
2025-03-11 14:52:10 +0100

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,16 @@
+# number1= 10
+# number2= 12.5
+# myDataSet= 123333
+# print(myDataSet)
+# print(number2)
+# print("Hello World")
+#Data set
+
+# myName = "Arowolo"
+# itemList = [myName, "Python", 2.34, False, 4]
+# print(itemList)
+
+calc = float(input("Enter a Number: "))
+calc2 = float(input("Enter a Number: "))
+result = calc * calc2
+print(result)
